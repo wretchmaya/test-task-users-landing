9e045bff0ffc9a00eb02cb75a33ba347937e5640
Bohdan Triska <Bohdan_Triska@epam.com>
2023-04-07 20:10:28 +0300

    perf imp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.less';
 import { LandingPage } from './pages/LandingPage/LandingPage';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Nunito Sans');
+  body {
+    font-family: 'Nunito Sans', sans-serif;
+  }
+`;
 
 function App() {
     return (
-        <div className="App">
-            <LandingPage />
-        </div>
+        <>
+            <GlobalStyle />
+            <div className="App">
+                <LandingPage />
+            </div>
+        </>
     );
 }
 
